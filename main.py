c8b9ace4d510a8298f56a4b8f662a4b611c6e21a
doranl <doranl@educ.college-montreal.qc.ca>
2024-09-17 15:46:07 -0400

    fini

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+"""
+Exercice: TP1
+Nom: Liam Doran
+Groupe: 404
+"""
+
+
 def word_count():
     count = str(input("écrit quelque chose\n"))
     count = count.split(" ")
     nombre_mot = len(count)
     print(nombre_mot)
+
+
 word_count()
