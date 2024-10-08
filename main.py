7e66a7fed241643f3f822da49cb6073ca082da40
doranl <doranl@educ.college-montreal.qc.ca>
2024-10-08 14:02:50 -0400

    fini

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ Groupe: 404
 def word_count():
     count = str(input("écrit quelque chose\n"))
     count = count.split(" ")
+    while "" in count:
+        count.remove("")
     nombre_mot = len(count)
     print(nombre_mot)
 
 
 word_count()
+
